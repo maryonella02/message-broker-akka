87d2a9e0dafb4eec8e46211e6e0a266a6d44c8dd
Marinela Branzeanu <maryonella02@gmail.com>
2022-06-21 21:35:01 +0300

    Add Subscriber class

---
+++ src/main/scala/com/example/Subscriber.scala
@@ -0,0 +1,27 @@
+package com.example
+
+import akka.actor.ActorRef
+
+import scala.collection.mutable.ListBuffer
+
+class Subscriber(sender: ActorRef) {
+  private val topics: ListBuffer[String] = ListBuffer[String]
+
+
+  def getSubscribedTopics: List[String] = topics.toList
+
+  def getActorRef: ActorRef = sender
+
+  def subscribeToTopic(topic: String): Unit = {
+    topics :+ topic
+  }
+
+  def unsubscribeFromTopic(topic: String): Unit = {
+    topics -= topic
+  }
+
+  def isSubscribedToTopic(topic: String): Boolean = {
+    topics.contains(topic)
+  }
+
+}
